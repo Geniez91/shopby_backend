d21289c8b5b9cbefc75902621e07f5c24034d1dc
Jeremy Weltmann <weltmannjeremy@gmail.com>
2025-12-16 17:13:25 +0100

    Initial commit

---
+++ src/main/java/org/shopby_backend/ShopbyBackendApplication.java
@@ -0,0 +1,13 @@
+package org.shopby_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopbyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopbyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/shopby_backend/ShopbyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.shopby_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopbyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
