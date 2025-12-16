6bf1b3d5354cf2777c2687dfbf35b02e43cfb475
Jeremy Weltmann <weltmannjeremy@gmail.com>
2025-12-16 17:31:37 +0100

    add security config and first controller

---
+++ src/main/java/org/shopby_backend/security/SecurityConfig.java
@@ -0,0 +1,15 @@
+package org.shopby_backend.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth->auth.anyRequest().permitAll());
+        return http.build();
+    }
+}
